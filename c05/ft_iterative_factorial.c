f9936da25f7e492bd5f244020ad4f1cabae78d88
kambarg <kambarg@gmail.com>
2024-03-19 06:32:23 +0000

    Update to ft_iterative_factorial. Added0! case

--- c05/ft_iterative_factorial.c
+++ c05/ft_iterative_factorial.c
@@ -5,19 +5,25 @@ int ft_iterative_factorial(int nb)
 {
     int res;
 
-    if (nb <= 0)
+    if (nb < 0)
     {
         return (0);
     }
-    res = 1;
-    while (nb > 0)
-    {
-        res = res * nb;
-        nb--;
-    }
-    return (res);
+    else if (nb == 0)
+		return (1);
+	else
+        res = 1;
+        while (nb > 0)
+        {
+            res = res * nb;
+            nb--;
+        }
+        return (res);
 }
 
+// 0! = 1
+// 3! = 3*2*1 = 6
+// 4! = 4*3*2*1 = 24
 int main (void)
 {
     int myn;
